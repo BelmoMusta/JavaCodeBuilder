a5dd22de0b94c15222cd8340d3da265f283df7da
musta <belmokhtar.91@gmail.com>
2018-03-14 02:34:59 +0000

    cleaning..

---
+++ src/test/java/objects/Person.java
@@ -0,0 +1,45 @@
+package objects;
+
+/**
+ * Created by DELL on 14/03/2018.
+ */
+public class Person {
+
+    private String name;
+    private String lastName;
+    private String address;
+    private String phone;
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+}
